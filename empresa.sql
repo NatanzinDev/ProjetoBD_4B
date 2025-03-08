7b7c02bee10f3df2161e644fe3ebe0ea1aa87995
Natan Paulo <natanpaulo1300@gmail.com>
2025-03-07 23:17:35 -0300

    finalizado2.0

--- empresa.sql
+++ empresa.sql
@@ -6,6 +6,21 @@ create table funcionario(
     salario double,
     cargo varchar(30),
     telefone varchar(20)
+    id_setor int, 
+    FOREIGN KEY (id_setor) REFERENCES setor(id_setor)
+);
+
+CREATE TABLE supervisor(
+id_supervisor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+email TEXT NOT NULL,
+senha TEXT NOT NULL,
+nome TEXT NOT NULL
+);
+
+create table setor(
+	id_setor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	nome varchar(100),
+	lugar varchar(100)
 );
 
 select * from funcionario;
